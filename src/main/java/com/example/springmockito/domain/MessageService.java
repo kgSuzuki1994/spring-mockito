7260b2d5ffbaaf35859a8cc77987abefc044e1da
kgSuzuki1994 <takuti0511@gmail.com>
2020-06-23 14:15:28 +0900

    get message from MessageSource

--- src/main/java/com/example/springmockito/domain/MessageService.java
+++ src/main/java/com/example/springmockito/domain/MessageService.java
@@ -1,10 +1,21 @@
 package com.example.springmockito.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 @Service
 public class MessageService {
+    @Autowired
+    MessageSource messageSource;
+
     public String getMessage() {
         return "Hello!";
+    }
+
+    public String getMessageByCode(String code) {
+        return messageSource.getMessage(code, null, Locale.getDefault());
     }
 }
--- src/test/java/com/example/springmockito/domain/MessageServiceTest.java
+++ src/test/java/com/example/springmockito/domain/MessageServiceTest.java
@@ -1,8 +1,14 @@
 package com.example.springmockito.domain;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.context.MessageSource;
+
+import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
 
 class MessageServiceTest {
 
@@ -10,6 +16,21 @@ class MessageServiceTest {
     void getMessage() {
         MessageService service = new MessageService();
         String actualMessage = service.getMessage();
+        assertEquals("Hello!", actualMessage);
+    }
+
+    @InjectMocks
+    MessageService service;
+    @Mock
+    MessageSource mockMessageSource;
+
+    @Test
+    void getMessageByCode() {
+        doReturn("Hello!").when(mockMessageSource)
+                .getMessage("greeting", null, Locale.getDefault());
+
+        // テスト実行
+        String actualMessage = service.getMessageByCode("greeting");
         assertEquals("Hello!", actualMessage);
     }
 }
